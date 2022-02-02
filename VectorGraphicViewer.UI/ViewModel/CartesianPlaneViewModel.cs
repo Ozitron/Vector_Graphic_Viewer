69919d38d9b7f790d2d4a0d70d54cf54ab86f235
Ozitron <ozankomurcu@gmail.com>
2022-02-03 00:16:18 +0300

    draw methods are updated

--- VectorGraphicViewer.UI/ViewModel/CartesianPlaneViewModel.cs
+++ VectorGraphicViewer.UI/ViewModel/CartesianPlaneViewModel.cs
@@ -1,14 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.Drawing;
 using System.Linq;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Shapes;
 using VectorGraphicViewer.UI.Command;
-using VectorGraphicViewer.UI.Model;
-using Line = VectorGraphicViewer.UI.Model.Line;
-using Point = System.Windows.Point;
+using Line = System.Windows.Shapes.Line;
 
 namespace VectorGraphicViewer.UI.ViewModel
 {
@@ -25,7 +23,6 @@ namespace VectorGraphicViewer.UI.ViewModel
                 _canvasWidth = value;
                 if (value > 0)
                     OnGridChanged(nameof(CanvasWidth));
-
             }
         }
 
@@ -46,9 +43,9 @@ namespace VectorGraphicViewer.UI.ViewModel
             }
         }
 
-        private readonly ObservableCollection<Path> _shapes;
+        private readonly ObservableCollection<Shape> _shapes;
 
-        public IEnumerable<Path> Shapes => _shapes;
+        public IEnumerable<Shape> Shapes => _shapes;
 
         public bool HasShapes => _shapes.Any();
 
@@ -63,7 +60,7 @@ namespace VectorGraphicViewer.UI.ViewModel
 
         public CartesianPlaneViewModel()
         {
-            _shapes = new ObservableCollection<Path>();
+            _shapes = new ObservableCollection<Shape>();
             ReadCommand = new RelayCommand(o => MainButtonClick());
             ClearCommand = new RelayCommand(o => ClearButtonClick());
 
@@ -84,79 +81,46 @@ namespace VectorGraphicViewer.UI.ViewModel
         {
             _shapes.Clear();
 
-            _shapes.Add(new Line(new Point(CanvasWidth / 2, 0),
-                new Point(CanvasWidth / 2, CanvasHeight), Color.Black).GetShape());
+            var xLine = new Line();
+            xLine.Stroke = Brushes.Black;
+            xLine.X1 = CanvasWidth / 2;
+            xLine.X2 = CanvasWidth / 2;
+            xLine.Y1 = 0;
+            xLine.Y2 = CanvasHeight;
+            xLine.StrokeThickness = 2;
+            _shapes.Add(xLine);
 
-            _shapes.Add(new Line(new Point(0, CanvasHeight / 2),
-                new Point(CanvasWidth, CanvasHeight / 2), Color.Black).GetShape());
+            var yLine = new Line();
+            yLine.Stroke = Brushes.Black;
+            yLine.X1 = 0;
+            yLine.X2 = CanvasWidth;
+            yLine.Y1 = CanvasHeight / 2;
+            yLine.Y2 = CanvasHeight / 2;
+            yLine.StrokeThickness = 2;
+            _shapes.Add(yLine);
 
             DrawShapes();
         }
 
         private void DrawShapes()
         {
-            //myLine = new System.Windows.Shapes.Line();
-            //myLine.Stroke = Brushes.Black;
-            //myLine.X1 = 0;
-            //myLine.X2 = CanvasWidth;
-            //myLine.Y1 = CanvasHeight / 2;
-            //myLine.Y2 = CanvasHeight / 2;
-            //myLine.StrokeThickness = 2;
-            //_shapes.Add(myLine);
+            //// Polygon
+            //var p = new Polygon();
+            //p.Stroke = Brushes.Black;
+            //p.Fill = Brushes.LightBlue;
+            //p.StrokeThickness = 2;
+            //p.Points = new PointCollection() { new Point(100, 100), new Point(45, 68), new Point(102, 135), new Point(220, 256) };
+            //_shapes.Add(p);
 
-            // line
-            //LineGeometry line = new LineGeometry();
-            //line.StartPoint = new System.Windows.Point(100, 150);
-            //line.EndPoint = new System.Windows.Point(200, 250);
-            //Path path = new Path();
-            //path.Stroke = Brushes.Red;
-            //path.StrokeThickness = 10;
-            //path.Data = line;
-            //_shapes.Add(path);
-
-            //var test =
-            //    new Model.Line(new System.Windows.Point(100, 150), new System.Windows.Point(200, 250), Color.Aqua);
-            //_shapes.Add(test.GetShape());
-
-            //// eliptical
-            ////var ellipse = new EllipseGeometry();
-            ////ellipse.Center = new Point(150, 150);
-            ////ellipse.RadiusX = 60;
-            ////ellipse.RadiusY = 40;
-            ////Path path2 = new Path();
-            ////path2.Stroke = Brushes.Red;
-            ////path2.Data = ellipse;
-            ////_shapes.Add(path2);
-
-            //LineGeometry line1 = new LineGeometry();
-            //line1.StartPoint = new Point(75, 80);
-            //line1.EndPoint = new Point(125, 150);
-            //LineGeometry line2 = new LineGeometry();
-            //line2.StartPoint = new Point(125, 150);
-            //line2.EndPoint = new Point(150, 120);
-            //LineGeometry line3 = new LineGeometry();
-            //line3.StartPoint = new Point(150, 120);
-            //line3.EndPoint = new Point(75, 80);
-            //GeometryGroup geometryGroup = new GeometryGroup();
-            //geometryGroup.Children.Add(line1);
-            //geometryGroup.Children.Add(line2);
-            //geometryGroup.Children.Add(line3);
-            //Path path3 = new Path();
-            //path3.Stroke = Brushes.BurlyWood;
-            //path3.StrokeThickness = 10;
-            //path3.Data = geometryGroup;
-            //_shapes.Add(path3);
-
-            var test = new Triangle(new Point(75, 80), new Point(125, 150), new Point(150, 120), new Color());
-
-            //var test = new Quadrilateral(new Point(75, 80),
-            //    new Point(125, 150),
-            //    new Point(150, 120),
-            //         new Point(200, 220),
-            //    Color.Red
-            //    );
-
-            _shapes.Add(test.GetShape());
+            //// Elliptical
+            //var ellipse = new EllipseGeometry();
+            //ellipse.Center = new Point(150, 150);
+            //ellipse.RadiusX = 60;
+            //ellipse.RadiusY = 40;
+            //var path2 = new Path();
+            //path2.Stroke = Brushes.Red;
+            //path2.Data = ellipse;
+            //_shapes.Add(path2);
         }
     }
 }
